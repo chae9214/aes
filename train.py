dbcff57a5654dba545327ac16ce030bdaf1fde41
Yoon Jaeyeun <dcpline14@gmail.com>
2017-12-05 03:19:55 +0900

    Change kappa to qudratic weighted kappa

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from torch.utils.data import DataLoader
 import numpy as np
 from scipy.stats import pearsonr
 from scipy.stats import spearmanr
-from sklearn.metrics import cohen_kappa_score
+from scipy.sparse import coo_matrix
 
 # =================================================
 # Utility functions
@@ -19,12 +19,33 @@ from sklearn.metrics import cohen_kappa_score
 def rmse(predictions, targets):
     return np.sqrt(((predictions - targets) ** 2).mean())
 
+def kappa_weights(n):
+    s = np.linspace(1, n, n)
+    w = np.power(np.subtract.outer(s, s), 2.0) / np.power(np.subtract.outer(n, np.ones((n,))), 2)
+    return w
+
+def quadratic_weighted_kappa(a, b, n = 20):
+    a = np.clip(a.astype('int32') - 1, 0, n - 1)
+    b = np.clip(b.astype('int32') - 1, 0, n - 1)
+
+    sa = coo_matrix((np.ones(len(a)), (np.arange(len(a)), a)), shape=(len(a), n))
+    sb = coo_matrix((np.ones(len(b)), (np.arange(len(b)), b)), shape=(len(a), n))
+
+    O = (sa.T.dot(sb)).toarray()
+    E = np.outer(sa.sum(axis=0), sb.sum(axis=0))
+    E = np.divide(E, np.sum(E)) * O.sum()
+    W = kappa_weights(n)
+
+    return 1.0 - np.multiply(O, W).sum() / np.multiply(E, W).sum()
+
 def get_metrics(y, y_):
     rmse_row = rmse(y, y_)
     r_row, p_value = pearsonr(y, y_)
     s_row, p_value = spearmanr(y, y_)
-    c_row = cohen_kappa_score(np.round(y), np.round(y_))
+    c_row = quadratic_weighted_kappa(np.round(y), np.round(y_), len(y_))
     return rmse_row, r_row, s_row, c_row
+
+
 
 # =================================================
 # Train function
